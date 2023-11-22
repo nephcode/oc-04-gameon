8b63d45f1b307a4583b7701bdc7dd79775c561e7
Néphélim <nephcohen@gmail.com>
2023-11-22 03:56:12 +0100

    Survivre
    sera dejà mon maximum

--- README.md
+++ README.md
@@ -23,11 +23,12 @@ A *JS FORM* validator animated project for OpenClassrooms course AKA Project 4 G
 
 - No library, Pure CSS/SASS, Pure JAVASCRIPT
 - [Figma ressources](https://www.figma.com/file/B7NKBDvSI18uoMLJgpnh48/UI-Design-GameOn-FR?node-id=106%3A630)
+- OpenClassCourse 100% completed
 
 ## EXPLAIN THE PROJECT
 - `HTML` Integration 
 - `CSS` to `SCSS` upgrade
-- 
+- Add `Defer` and replace `<script>` in **HEAD**
 
 
 ![cover](https://kpkfzczpavanzocxzyta.supabase.co/storage/v1/object/public/nephcode-public/githubReadmeSkills.png)
--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,6 @@
+// ============================================================
+// NAV ========================================================
+// ============================================================
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,10 +10,15 @@ function editNav() {
   }
 }
 
-// DOM Elements
+// ============================================================
+// MODAL ACTION ===============================================
+// ============================================================
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+
+let gamePassFirst = document.getElementById("first").value;
+console.log(gamePassFirst);
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -18,17 +26,64 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+  console.log("Ouverture de la modal");
 }
 
 
 // Sélectionnez l'élément qui représente la croix
 const closeBtn = document.querySelector('.close');
-
-// La modale que vous souhaitez fermer
 const modal = document.querySelector('.bground');
 
 // Ajoutez un écouteur d'événements pour détecter le clic sur la croix
 closeBtn.addEventListener('click', () => {
   // Ajoutez la logique pour cacher la modale
   modal.style.display = 'none';
+  console.log("Fermeture de la modal");
+});
+
+// ============================================================
+// FORMULAIRE  ================================================
+// ============================================================
+
+
+// PRENOM =====================================================
+let gameFirst = document.getElementById("first").value;
+if (gameFirst) {
+  gameFirst.addEventListener('blur', () => {
+      console.log(gameFirst);
+  });
+} else {
+  gameFirstStyle = document.getElementById("first");
+  gameFirstStyle.classList.add(".text-control");
+  console.log("L'élément 'first' est vide.");
+  
+}
+// NOM ========================================================
+let gameLast = document.getElementById("last").value;
+if (gameLast) {
+  gameLast.addEventListener('blur', () => {
+      console.log(gameLast);
+  });
+} else {
+  console.log("L'élément 'last' est vide.");
+}
+
+
+//const gameLast = document.getElementById("last").value;
+const gameBirth = document.getElementById("birthdate").value;
+const gameEmail = document.getElementById("email").value;
+const gameQuantity = document.getElementById("quantity").value;
+console.log(gameFirst);
+console.log(gameLast);
+
+//=====================
+//=====================
+//=====================
+
+form.addEventListener("submit", (event) => {
+  // On empêche le comportement par défaut
+  event.preventDefault();
+  console.log("Il n’y a pas eu de rechargement de page");
+
+
 });
